fb1f9d20f021c54b153f361cc7e3818e4fafdaee
Richmond <richee305@yahoo.ca>
2015-01-01 22:08:33 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+  
+  def create
+	@user = User.new(params[:users])
+  	if @user.save
+	  #Suucess creating account
+	    #redirect_to @user
+	 else
+	   render 'new'
+	end     
+  end
+  
+  def create
+	@user = User.new(params[:user])
+  	if @user.save
+	  #Suucess creating accou
+	    flash[:success] = "Welcome to the Forum Page for CSE 3482"
+	    redirect_to @user
+	 else
+	   render 'new'
+	end     
+  end
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "Richmond"
+		email "richee@example.com"
+		password "SixSide"
+		password_confirmation "SixSide"
+	end
+end	
